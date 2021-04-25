5aafdd3fa28c4037a33b1c04a34355d2e43d9898
Jon Burton <jonburto@gmail.com>
2021-04-25 15:49:56 +0100

    Create GameStateTests.cs

---
+++ SheddingCardGames/SheddingCardGames.Tests/Domain/GameStateTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using SheddingCardGames.Domain;
+using SheddingCardGames.UiLogic;
+using Xunit;
+
+namespace SheddingCardGames.Tests.Domain
+{
+    namespace GameStateTests
+    {
+        public class NextPlayerShould
+        {
+            private readonly SampleData sampleData;
+
+            public NextPlayerShould()
+            {
+                sampleData = new SampleData();
+            }
+
+            private GameState CreateSut(Player playerToPlay)
+            {
+                return new GameState
+                {
+                    CurrentTable = new Table(new StockPile(new CardCollection()), new DiscardPile(), sampleData.Player1,
+                        sampleData.Player2),
+                    CurrentTurn = new CurrentTurn(1, playerToPlay, new Card[0], Action.Play)
+                };
+            }
+
+            [Fact]
+            public void ReturnNextPlayer()
+            {
+                var sut = CreateSut(sampleData.Player1);
+
+                var actual = sut.NextPlayer;
+
+                actual.Should().Be(sampleData.Player2);
+            }
+
+            [Fact]
+            public void ReturnFirstPlayerAfterLastPlayer()
+            {
+                var sut = CreateSut(sampleData.Player2);
+
+                var actual = sut.NextPlayer;
+
+                actual.Should().Be(sampleData.Player1);
+            }
+        }
+    }
+}
