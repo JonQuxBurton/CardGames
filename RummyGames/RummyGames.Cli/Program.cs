c0dc68fbcc01fd7cb8782a9d5c5acaa59b82c038
Jon Burton <jonburto@gmail.com>
2022-01-18 15:08:33 +0000

    Add CLI

---
+++ RummyGames/RummyGames.Cli/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RummyGames.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataStore = new DataStore();
+            var lobby = new Lobby(dataStore);
+            var player1 = new Player(Guid.NewGuid(), "Alice");
+
+            var game = lobby.CreateGame(player1);
+
+            var player2 = new Player(Guid.NewGuid(), "Bob");
+
+            game = lobby.JoinGame(game, player2);
+
+
+            Console.WriteLine("Rummy");
+            Console.WriteLine($"Host: {game.Host.Name}, Guest: {game.Guest.Name}");
+        }
+    }
+}
